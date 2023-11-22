4c7f4750557845a06bb9dd32aa14a707790448f3
hehusong <2995991652@qq.com>
2023-11-22 11:09:26 +0800

    init

---
+++ Demo.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: Demo
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author hehusong
+ * @Create 2023/11/22 11:08
+ * @Version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
